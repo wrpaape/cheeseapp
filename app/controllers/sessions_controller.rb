6fa31f48b87ebcfc8b677c0e3a0cfa66a2d83cb6
Reid Paape <wrpaape@gmail.com>
2015-06-23 16:15:15 -0500

    updated controllers to render views, not json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,30 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user User.find_by(name: params[:name]) && user.authenticate(params[:password])
+      # If the user exists AND the password entered is correct.
       # Save the user id inside a browser cookie.
       # Specifically, the rails 'session'. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      render_response({ response: "successfully logged in" }, 200)
+      redirect_to root_url, notice: 'successfully logged in'
     elsif user
       # If user's login doesn't work, send them back to the login form.
-      render_response({ response: "incorrect password" }, 422)
+      flash[:alert] = 'incorrect password'
+      render :new
     else
-      render_response({ response: "username does not exist" }, 422)
+      flash[:alert] = 'username or email does not exist'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    render_response({ response: "successfully logged out" }, 200)
-  end
-
-  private
-
-  def render_response(response, response_code)
-    render json: response, status: response_code
+    redirect_to root_url, notice: 'successfully logged out'
   end
 end
